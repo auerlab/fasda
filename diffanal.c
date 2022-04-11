7329723652610b5aa610459c2c2261a85feffcdf
Jason Bacon <bacon@NetBSD.org>
2022-04-11 11:12:34 -0500

    Fix build on macOS

--- diffanal.c
+++ diffanal.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>         // ftruncate()
 #include <sys/param.h>      // MIN(), MAX()
 #include <xtend/file.h>
 #include <xtend/string.h>   // strlcpy() on Linux
