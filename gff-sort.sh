70f17d7040d3847ff99e19461bcecef354e4a7e4
Jason Bacon <bacon@moray.acadix.biz>
2022-04-12 07:18:37 -0500

    Fix typo

--- gff-sort.sh
+++ gff-sort.sh
@@ -4,5 +4,5 @@ awk '$3 == "gene" && $1 != "MT" && $1 != "X" && $1 != "Y"' Data/test.gff3 \
     | sort -n -k 1 -k 4 > Data/genes-sorted.gff3
 awk '$3 == "mRNA" && $1 != "MT" && $1 != "X" && $1 != "Y"' Data/test.gff3 \
     | sort -n -k 1 -k 4 > Data/mRNA-sorted.gff3
-awk '{ print $1 }' Data/test-sorted.gff3 | uniq
+awk '{ print $1 }' Data/mRNA-sorted.gff3 | uniq
 
