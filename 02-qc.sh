3511659b5d73c2b973f69cb02e2231f21a6f4f92
Jason Bacon <bacon@moray.acadix.biz>
2022-05-12 14:58:48 -0500

    Streamline QC script

--- 02-qc.sh
+++ 02-qc.sh
@@ -1,29 +1,18 @@
 #!/bin/sh -e
 
-report_dir=Data/02-qc/Raw
-mkdir -p $report_dir
-for file in Data/Yeast/Raw-renamed/*.fastq.gz; do
-    stem=`basename ${file%.fastq.gz}`
-    report=$report_dir/${stem}_fastqc.zip
-    if [ -e $report ]; then
-	printf "$report already exists.\n"
-    else
-	printf "Generating $report...\n"
-	fastqc --outdir $report_dir $file
-    fi
+for fir in Raw-renamed Trimmed; do
+    report_dir=Data/02-qc/$dir
+    mkdir -p $report_dir
+    for file in Data/Yeast/$dir/*.fastq.gz; do
+	stem=`basename ${file%.fastq.gz}`
+	report=$report_dir/${stem}_fastqc.zip
+	if [ -e $report ]; then
+	    printf "$report already exists.\n"
+	else
+	    printf "Generating $report...\n"
+	    fastqc --outdir $report_dir $file
+	fi
+    done
+    multiqc --outdir $report_dir $report_dir
+    firefox $report_dir/multiqc_report.html || true
 done
-multiqc --outdir $report_dir $report_dir
-
-report_dir=Data/02-qc/Trimmed
-mkdir -p $report_dir
-for file in Data/01-trim/*.fastq.gz; do
-    stem=`basename ${file%.fastq.gz}`
-    report=$report_dir/${stem}_fastqc.zip
-    if [ -e $report ]; then
-	printf "$report already exists.\n"
-    else
-	printf "Generating $report...\n"
-	fastqc --outdir $report_dir $file
-    fi
-done
-multiqc --outdir $report_dir $report_dir
