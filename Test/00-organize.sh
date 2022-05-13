970a2f103a4e516eba18b2eca6a4d9a793b6988f
Jason Bacon <bacon@moray.acadix.biz>
2022-05-13 17:36:37 -0500

    00-organize.sh: Improve help message

--- Test/00-organize.sh
+++ Test/00-organize.sh
@@ -22,16 +22,18 @@ if [ "$(ls WT-* | wc -l)" -lt 8 ] \
     || [ "$(ls SNF2-* | wc -l)" -lt 8 ]; then
     cat << EOM
 
-You must download at least 8 replicates of WT and 8 replicates of
-of SNF2 RNA-Seq data from
+You must download at least 8 biological replicates of WT and 8 biological
+replicates of SNF2 RNA-Seq data from
 
-https://www.ebi.ac.uk/ena/browser/view/PRJEB5348
+    https://www.ebi.ac.uk/ena/browser/view/PRJEB5348
 
-Sample names are mapped in ERP004763_sample_mapping.tsv.
+and place them in the Yeast directory.
+
+Sample names are mapped in ERP004763_sample_mapping.tsv.  You can select
+sample names at the link above and download the lot of them as a .zip file.
 
 EOM
     printf "WT-*:   $(ls WT-* | wc -l)\n"
     printf "SNF2-*: $(ls SNF2-* | wc -l)\n"
     exit 1
 fi
-
