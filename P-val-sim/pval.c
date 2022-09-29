79adcfb8d80f3415d950162ae9cf72f4a464c6fe
Jason W. Bacon <bacon4000@gmail.com>
2022-09-29 07:50:51 -0500

    Clean up P-val sim and notes

--- P-val-sim/pval.c
+++ P-val-sim/pval.c
@@ -29,14 +29,14 @@ int     main(int argc,char *argv[])
 {
     int     c, c1, c2, pairs = 0, fc_ge = 0, replicates = 3,
 	    less = 0, more = 0, equal = 0, *counts,
-	    count1_avg, count2_avg;
-    double  fc, avg_fc, slop;
+	    count1_mean, count2_mean;
+    double  fc, mean_fc, slop;
 
     if ( argc != 5 )
 	usage(argv);
     
-    count1_avg = atoi(argv[1]);
-    count2_avg = atoi(argv[2]);
+    count1_mean = atoi(argv[1]);
+    count2_mean = atoi(argv[2]);
     slop = atof(argv[3]);
     replicates = atoi(argv[4]);
     
@@ -55,28 +55,32 @@ int     main(int argc,char *argv[])
      *  Generate random counts with FC around 2
      */
     printf("\ncount1 = %d +/- to up to %0.0f%%, count2 = %d +/- same\n",
-	    count1_avg, slop * 100, count2_avg);
-    puts("Cond1  Cond2");
+	    count1_mean, slop * 100, count2_mean);
+    puts("Cond1 Cond2");
     srandom(time(NULL));
-    for (c = 0, avg_fc = 0.0; c < replicates; ++c)
+    for (c = 0, mean_fc = 0.0; c < replicates; ++c)
     {
-	counts[c] = count1_avg
-		 + random() % (int)(count1_avg * slop * 2) - count1_avg * slop;
-	counts[c + replicates] = count2_avg
-		 + random() % (int)(count2_avg * slop * 2) - count2_avg * slop;
+	counts[c] = count1_mean
+		 + random() % (int)(count1_mean * slop * 2) - count1_mean * slop;
+	counts[c + replicates] = count2_mean
+		 + random() % (int)(count2_mean * slop * 2) - count2_mean * slop;
 	fc = (double)counts[c + replicates] / counts[c];
-	printf("%3d %3d %f\n", counts[c], counts[c + replicates], fc);
-	avg_fc += fc;
+	printf("%5d %5d %f\n", counts[c], counts[c + replicates], fc);
+	mean_fc += fc;
     }
-    avg_fc /= replicates;
-    printf("Average FC = %f\n", avg_fc);
+    mean_fc /= replicates;
+    printf("Mean FC = %f\n", mean_fc);
+    
+    puts("\nPooled counts:");
+    for (c1 = 0; c1 < replicates * 2; ++c1)
+	printf("%3d\n", counts[c1]);
     
     /*
      *  Compute fold-change for every possible random pairing.
      *  Satisfies the null hypothesis P(n1 > n2) = P(n1 < N2)
      */
     puts("\nFold-change of every possible pairing of pooled samples:");
-    puts("(Except counts are not paired with themselves.)");
+    puts("(Counts are not paired with themselves.)");
     for (c1 = 0; c1 < replicates * 2; ++c1)
 	for (c2 = 0; c2 < replicates * 2; ++c2)
 	{
@@ -84,8 +88,8 @@ int     main(int argc,char *argv[])
 	    {
 		++pairs;
 		fc = (double)counts[c1] / counts[c2];
-		printf("%0.1f ", fc);
-		if ( fc >= avg_fc )
+		printf("%3d / %3d = %0.1f\n", counts[c1], counts[c2], fc);
+		if ( fc >= mean_fc )
 		    ++fc_ge;
 		
 		if ( counts[c1] < counts[c2] )
@@ -98,7 +102,7 @@ int     main(int argc,char *argv[])
 	}
     
     printf("\n\nTotal pairings = %d  FC >= %f = %d  P(FC >= %f) = %f\n",
-	    pairs, avg_fc, fc_ge, avg_fc, (double)fc_ge / pairs);
+	    pairs, mean_fc, fc_ge, mean_fc, (double)fc_ge / pairs);
     
     printf("\nDistribution: less = %d  more = %d  equal = %d\n",
 	    less, more, equal);
