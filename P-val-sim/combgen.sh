dba2ea5fa35da241b381cb756c2c87bd104865f1
Jason W. Bacon <bacon4000@gmail.com>
2022-09-30 08:55:01 -0500

    combgen.sh: Generate optimal n choose k loops for various sizes

---
+++ P-val-sim/combgen.sh
@@ -0,0 +1,100 @@
+##########################################################################
+#   Synopsis:
+#       
+#   Description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#
+#   Examples:
+#
+#   Files:
+#
+#   Environment:
+#
+#   See also:
+#       
+#   History:
+#   Date        Name        Modification
+#   2022-09-30  Jason Bacon Begin
+##########################################################################
+
+usage()
+{
+    printf "Usage: $0 n k\n"
+    exit 1
+}
+
+
+##########################################################################
+#   Function description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#       
+#   History:
+#   Date        Name        Modification
+#   2022-09-30  Jason Bacon Begin
+##########################################################################
+
+print_indent()
+{
+    if [ $# != 1 ]; then
+	printf "Usage: indent n\n"
+	exit 1
+    fi
+    c=$1
+    
+    for space in $(seq 1 $((c - 1))); do
+	printf " "
+    done
+}
+
+
+##########################################################################
+#   Main
+##########################################################################
+
+if [ $# != 2 ]; then
+    usage
+fi
+n=$1
+k=$2
+
+cat << EOM
+/*
+ *  Generate all combinations n choose $k
+ */
+
+void    combinations$k(size_t n)
+
+{
+EOM
+
+# Variable defs
+printf "    size_t  "
+for c in $(seq 1 $((k - 1))); do
+    printf "c%d, " $c
+done
+printf "c%d;\n\n" $k
+
+# Nested loop
+printf "    for (c1 = 0; c1 < n; ++c1)\n"
+for c in $(seq 2 $k); do
+    print_indent $c
+    printf "    for (c$c = c$((c - 1)) + 1; c$c < n; ++c$c)\n"
+done
+
+# Body
+print_indent $c
+printf "    {\n"
+print_indent $c
+printf "        fc_count();\n"
+print_indent $c
+printf "    }\n"
+
+# Closing braces
+printf "}\n"
+
