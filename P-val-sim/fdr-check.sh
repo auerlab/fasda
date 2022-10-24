f634b006a35db087047ff7e397c5fca6d8107123
Jason W. Bacon <bacon4000@gmail.com>
2022-10-24 11:53:15 -0500

    fdr-check.sh: Clean up output

--- P-val-sim/fdr-check.sh
+++ P-val-sim/fdr-check.sh
@@ -12,8 +12,12 @@ for p_val in 0.05 0.01; do
     esac
     rm -f tmp*.txt
     for replicates in $(seq 3 10); do
+	printf "P-value: $p_val  Replicates: $replicates  "
+	printf "Expected false positives = %s\n" \
+	    $(printf "$p_val * $runs\nquit\n" | bc -l)
+	
 	# Run expensive P-value computations in parallel
-	seq 1 4 | parallel ./pval 100 100 .5 $replicates $runs '>' tmp{}.txt
+	seq 1 4 | parallel ./pval-sim 100 100 .5 $replicates $runs '>' tmp{}.txt
 	
 	for i in $(seq 1 4); do
 	    awk '$1 == "Exact" && $4 <= '$p_val' { print $4 }' tmp$i.txt \
@@ -27,23 +31,22 @@ for p_val in 0.05 0.01; do
 	for v in $exact_counts; do
 	    exact_sum=$((exact_sum + v))
 	done
-
-	mw_counts=$(cat mw-output.*)
-	mw_sum=0
-	for v in $mw_counts; do
-	    mw_sum=$((mw_sum + v))
-	done
-	
-	printf "Expected false positives = %s\n" \
-	    $(printf "$p_val * $runs\nquit\n" | bc -l)
-	printf "P-value: $p_val  Replicates: $replicates  "
-	printf "Exact average of $i x $runs trials: $((exact_sum / i))\n"
-	printf "Exact counts for each run: "
+	printf "Exact P-val counts for each run: "
 	printf "%s " $exact_counts
-	printf "\nMann-whitney average of $i x $runs trials: $((mw_sum / i))\n"
-	printf "Mann-Whitney counts for each run: "
-	printf "%s " $mw_counts
-	printf "\n\n"
+	printf "\nExact P-val average of $i x $runs trials: $((exact_sum / i))\n"
+	
+	if [ $replicates -ge 8 ]; then
+	    mw_counts=$(cat mw-output.*)
+	    mw_sum=0
+	    for v in $mw_counts; do
+		mw_sum=$((mw_sum + v))
+	    done
+	    printf "Mann-Whitney counts for each run: "
+	    printf "%s " $mw_counts
+	    printf "\nMann-Whitney average of $i x $runs trials: $((mw_sum / i))\n"
+	fi
+	
+	printf "\n"
     done
     rm -f tmp*.txt exact-output.* mw-output.*
 done
