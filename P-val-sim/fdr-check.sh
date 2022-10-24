6e914245b266c070e6dd82c10cc325b905f4c449
Jason W. Bacon <bacon4000@gmail.com>
2022-10-24 12:07:45 -0500

    fdr-check.sh: Clean up

--- P-val-sim/fdr-check.sh
+++ P-val-sim/fdr-check.sh
@@ -4,26 +4,26 @@ make
 for p_val in 0.05 0.01; do
     case $p_val in
     0.10|0.05)
-	runs=1000
+	trials=1000
 	;;
     0.01)
-	runs=10000
+	trials=10000
 	;;
     esac
     rm -f tmp*.txt
     for replicates in $(seq 3 10); do
 	printf "P-value: $p_val  Replicates: $replicates  "
 	printf "Expected false positives = %s\n" \
-	    $(printf "$p_val * $runs\nquit\n" | bc -l)
+	    $(printf "$p_val * $trials\nquit\n" | bc -l)
 	
 	# Run expensive P-value computations in parallel
-	seq 1 4 | parallel ./pval-sim 100 100 .5 $replicates $runs '>' tmp{}.txt
+	seq 1 4 | parallel ./pval-sim 100 100 .5 $replicates $trials '>' tmp{}.txt
 	
-	for i in $(seq 1 4); do
-	    awk '$1 == "Exact" && $4 <= '$p_val' { print $4 }' tmp$i.txt \
-		| wc -l > exact-output.$i
-	    awk '$1 == "Exact" && $7 <= '$p_val' { print $7 }' tmp$i.txt \
-		| wc -l > mw-output.$i
+	for run in $(seq 1 4); do
+	    awk '$1 == "Exact" && $4 <= '$p_val' { print $4 }' tmp$run.txt \
+		| wc -l > exact-output.$run
+	    awk '$1 == "Exact" && $7 <= '$p_val' { print $7 }' tmp$run.txt \
+		| wc -l > mw-output.$run
 	done
 	
 	exact_counts=$(cat exact-output.*)
@@ -31,9 +31,9 @@ for p_val in 0.05 0.01; do
 	for v in $exact_counts; do
 	    exact_sum=$((exact_sum + v))
 	done
-	printf "Exact P-val counts for each run: "
+	printf "Exact P-val counts for each run of $trials trials: "
 	printf "%s " $exact_counts
-	printf "\nExact P-val average of $i x $runs trials: $((exact_sum / i))\n"
+	printf "\nExact P-val average of $run x $trials trials: $(($exact_sum / $run))\n"
 	
 	if [ $replicates -ge 8 ]; then
 	    mw_counts=$(cat mw-output.*)
@@ -41,9 +41,9 @@ for p_val in 0.05 0.01; do
 	    for v in $mw_counts; do
 		mw_sum=$((mw_sum + v))
 	    done
-	    printf "Mann-Whitney counts for each run: "
+	    printf "Mann-Whitney counts for each run of $trials trials: "
 	    printf "%s " $mw_counts
-	    printf "\nMann-Whitney average of $i x $runs trials: $((mw_sum / i))\n"
+	    printf "\nMann-Whitney average of $run x $trials trials: $(($mw_sum / $run))\n"
 	fi
 	
 	printf "\n"
