b8c3c63a4ea6bb52b26fb92042f5cef34a2e0253
Jason W. Bacon <bacon4000@gmail.com>
2022-11-04 19:10:54 -0500

    Test: Add hisat2 alignment scripts

---
+++ Test/06-hisat2-index.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Description:
+#       Build hisat2 index for reference genome.
+#
+#       All necessary tools are assumed to be in PATH.  If this is not
+#       the case, add whatever code is needed here to gain access.
+#       (Adding such code to your .bashrc or other startup script is
+#       generally a bad idea since it's too complicated to support
+#       every program with one environment.)
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-11-24  Jason Bacon Begin
+##########################################################################
+
+# Document software versions used for publication
+uname -a
+hisat2 --version
+samtools --version
+pwd
+
+# Run hisat2-build on a copy in 15-hisat2-index so it will put the .ht2
+# files there
+genome=$(Reference/genome-filename.sh)
+ln -f Data/03-reference/$genome Data/06-hisat2-index
+genome=Data/06-hisat2-index/$genome
+printf "Using reference $genome...\n"
+
+if [ ! -e $genome.8.ht2 ]; then
+    printf "Building $genome.*.ht2...\n"
+    hisat2-build $genome $genome
+fi
+if [ ! -e $genome.fai ]; then
+    printf "Building $genome.fai...\n"
+    samtools faidx $genome
+fi
+ls Data/06-hisat2-index
---
+++ Test/07-hisat2-align.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Description:
+#       Run hisat2 aligner on each RNA sample.
+#
+#       All necessary tools are assumed to be in PATH.  If this is not
+#       the case, add whatever code is needed here to gain access.
+#       (Adding such code to your .bashrc or other startup script is
+#       generally a bad idea since it's too complicated to support
+#       every program with one environment.)
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-11-24  Jason Bacon Begin
+##########################################################################
+
+##############################################################################
+# Align with hisat2, which can handle splice junctions in RNA reads
+
+# Document software versions used for publication
+uname -a
+hisat2 --version
+pwd
+
+build=$(Common/genome-build.sh)
+release=$(Common/genome-release.sh)
+genome=$(Reference/genome-filename.sh)
+
+# samtools sort dumps temp files in CWD
+cd Data/07-hisat2-align
+
+for sample in ../01-trim/*; do
+    gz1=$(ls $sample)
+    gzb=$(basename $gz1)
+    bam=${gzb%-R*}.bam
+    
+    set -x
+    hisat2 --threads 2 -x ../06-hisat2-index/$genome \
+	-U $gz1 | samtools sort > $bam
+    samtools index $bam
+done
