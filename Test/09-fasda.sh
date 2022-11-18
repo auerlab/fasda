259249ff08792b825c9228320fa683031ec741b6
Jason W. Bacon <bacon4000@gmail.com>
2022-11-18 17:39:36 -0600

    09-fasda.sh: Run more sample sizes for both NE and MW

--- Test/09-fasda.sh
+++ Test/09-fasda.sh
@@ -30,44 +30,71 @@ for condition in WT SNF2; do
     r=$(ls $kallisto_dir/$condition-*/abundance.tsv | wc -l)
     tr=$(echo $r)
     printf "Normalizing $condition: $tr replicates\n"
-    time fasda normalize --output $condition-all-norm.tsv \
+    time fasda normalize --output $condition-all-norm-$tr.tsv \
 	$kallisto_dir/$condition-*/abundance.tsv
 done
 
 printf "Computing fold-change...\n"
 time fasda fold-change --output WT-SNF2-FC-MW-$tr.txt \
-    WT-all-norm.tsv SNF2-all-norm.tsv
-pause
+    WT-all-norm-$tr.tsv SNF2-all-norm-$tr.tsv
 head WT-SNF2-FC-MW-$tr.txt
 
-printf "Compute near-exact P-values for $tr replicates? y/[n] "
-read ne
-if [ 0$ne = 0y ]; then
-    time fasda fold-change --near-exact --output WT-SNF2-FC-NE-$tr.txt \
-	WT-all-norm.tsv SNF2-all-norm.tsv
-fi
-if [ -e WT-SNF2-FC-NE-$tr.txt ]; then
-    head WT-SNF2-FC-MW-$tr.txt WT-SNF2-FC-NE-$tr.txt
-    pause
-fi
-
 ##########################################################################
-#   3 - 7 replicates, should use exact P-values
+#   3 to 12 replicates, [near-]exact P-values
 ##########################################################################
 
-for replicates in $(seq 3 7); do
+if [ $tr -lt 12 ]; then
+    max_ne=$tr
+else
+    max_ne=12
+fi
+
+for replicates in $(seq 3 $max_ne); do
     for condition in WT SNF2; do
-	printf "Normalizing $condition: $replicates replicates\n"
-	time fasda normalize --output \
-	    $condition-all-norm-$replicates.tsv \
-	    $kallisto_dir/$condition-[1-$replicates]/abundance.tsv
+	if [ ! -e $condition-all-norm-$replicates.tsv ]; then
+	    printf "Normalizing $condition: $replicates replicates\n"
+	    time fasda normalize --output \
+		$condition-all-norm-$replicates.tsv \
+		$kallisto_dir/$condition-[1-$replicates]/abundance.tsv
+	fi
     done
     
-    printf "Computing fold-change for $replicates replicates...\n"
-    time fasda fold-change --output WT-SNF2-FC-NE-$replicates.txt \
-	WT-all-norm-$replicates.tsv SNF2-all-norm-$replicates.tsv
-    pause
+    if [ ! -e WT-SNF2-FC-NE-$replicates.txt ]; then
+	if [ ! -e WT-SNF2-FC-NE-$replicates.txt ]; then
+	    printf "Computing fold-change for $replicates replicates...\n"
+	    time fasda fold-change --near-exact \
+		--output WT-SNF2-FC-NE-$replicates.txt \
+		WT-all-norm-$replicates.tsv SNF2-all-norm-$replicates.tsv
+	fi
+    fi
+done
+
+##########################################################################
+#   8 to all replicates, Mann-Whitney P-values
+##########################################################################
+
+for replicates in $(seq 8 $max_ne) `seq $(($max_ne + 1)) 5 $tr`; do
+    for condition in WT SNF2; do
+	if [ ! -e $condition-all-norm-$replicates.tsv ]; then
+	    printf "Normalizing $condition: $replicates replicates\n"
+	    time fasda normalize --output \
+		$condition-all-norm-$replicates.tsv \
+		$kallisto_dir/$condition-[1-$replicates]/abundance.tsv
+	fi
+    done
     
-    head WT-SNF2-FC-MW-$tr.txt WT-SNF2-FC-NE-$tr.txt WT-SNF2-FC-NE-*.txt | more
-    pause
+    if [ ! -e WT-SNF2-FC-MW-$replicates.txt ]; then
+	if [ ! -e WT-SNF2-FC-MW-$replicates.txt ]; then
+	    printf "Computing fold-change for $replicates replicates...\n"
+	    time fasda fold-change \
+		--output WT-SNF2-FC-MW-$replicates.txt \
+		WT-all-norm-$replicates.tsv SNF2-all-norm-$replicates.tsv
+	fi
+    fi
+done
+    
+head WT-SNF2-FC-NE-*.txt WT-SNF2-FC-MW-*.txt | more
+printf "%-25s %10s %10s\n" "File" "Features" "P < 0.05"
+for file in WT-SNF2-FC-NE-*.txt WT-SNF2-FC-MW-*.txt; do
+    printf "%-25s %10s %10s\n" $file: $(cat $file | wc -l) $(awk '$5 < 0.05' $file | wc -l)
 done
