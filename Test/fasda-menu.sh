e30c761fff45158ff95a000c820a5207c62670a7
Jason W. Bacon <bacon4000@gmail.com>
2022-11-22 19:48:14 -0600

    Frame out menu-based scripts for browsing results

---
+++ Test/fasda-menu.sh
@@ -0,0 +1,75 @@
+#!/bin/sh -e
+
+
+##########################################################################
+#   Function description:
+#       Pause until user presses return
+##########################################################################
+
+pause()
+{
+    local junk
+    
+    printf "Press return to continue..."
+    read junk
+}
+
+
+while true; do
+    clear
+    cat << EOM
+
+			_________   _____ ____  ___ 
+		       / ____/   | / ___// __ \/   |
+		      / /_  / /| | \__ \/ / / / /| |
+		     / __/ / ___ |___/ / /_/ / ___ |
+		    /_/   /_/  |_/____/_____/_/  |_|
+
+1.. View alignment counts for one feature
+2.. View alignment counts based on DA results
+Q.. Quit
+
+EOM
+    printf "Selection? "
+    read selection
+    case $selection in
+    1)
+	printf "Feature name? "
+	read feature
+	for replicates in $(seq 3 48); do
+	    pr=$(printf "%02s" $replicates)
+	    if [ -e Data/09-fasda/WT-all-norm-$pr.tsv ]; then
+		./show-counts.sh $feature $replicates | more
+	    fi
+	done
+	;;
+    
+    2)
+	printf "Replicates? "
+	read replicates
+	printf "Maximum P-value? "
+	read pval
+	printf "Minimum fold-change? "
+	read fc
+	
+	pr=$(printf "%02s" $replicates)
+	da_file=Data/09-fasda/WT-SNF2-FC-NE-$pr.txt
+	if [ -e $da_file ]; then
+	    awk -v pval=$pval -v fc=$fc \
+		'$8 <= pval && $7 >= fc { print $0 }' \
+		$da_file | more
+	else
+	    printf "$da_file: No such file.\n"
+	    pause
+	fi
+	;;
+    
+    Q|q)
+	exit 0
+	;;
+    
+    *)
+	printf "Invalid selection.\n" >> /dev/stderr
+	;;
+    esac
+done
---
+++ Test/show-counts.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Synopsis:
+#       
+#   Description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#
+#   Examples:
+#
+#   Files:
+#
+#   Environment:
+#
+#   See also:
+#       
+#   History:
+#   Date        Name        Modification
+#   2022-11-22  Jason Bacon Begin
+##########################################################################
+
+usage()
+{
+    printf "Usage: $0 feature-name replicates\n"
+    exit 1
+}
+
+
+##########################################################################
+#   Main
+##########################################################################
+
+if [ $# != 2 ]; then
+    usage
+fi
+feature=$1
+replicates=$(printf "%02s" $2)
+
+printf "Replicates = $replicates\n"
+awk -v feature=$feature '$1 == feature { print $0 }' \
+    Data/09-fasda/WT-all-norm-$replicates.tsv \
+    Data/09-fasda/SNF2-all-norm-$replicates.tsv
+
