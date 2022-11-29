ea58acc70b5bd6da71bcd7b2cb20e3519dad250d
Jason W. Bacon <bacon4000@gmail.com>
2022-11-29 07:47:40 -0600

    Improve test script documentation

--- Test/01-fetch.sh
+++ Test/01-fetch.sh
@@ -3,6 +3,12 @@
 ##########################################################################
 #   Description:
 #       Fetch Yeast sample data and create symlinks with descriptive names
+#
+#       All necessary tools are assumed to be in PATH.  If this is not
+#       the case, add whatever code is needed here to gain access.
+#       (Adding such code to your .bashrc or other startup script is
+#       generally a bad idea since it's too complicated to support
+#       every program with one environment.)
 #       
 #   History:
 #   Date        Name        Modification
--- Test/03-qc.sh
+++ Test/03-qc.sh
@@ -3,6 +3,12 @@
 ##########################################################################
 #   Description:
 #       Run fastqc on raw and trimmed reads
+#
+#       All necessary tools are assumed to be in PATH.  If this is not
+#       the case, add whatever code is needed here to gain access.
+#       (Adding such code to your .bashrc or other startup script is
+#       generally a bad idea since it's too complicated to support
+#       every program with one environment.)
 #       
 #   History:
 #   Date        Name        Modification
