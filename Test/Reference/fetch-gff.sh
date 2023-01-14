fd3f7b53beda4decbfb54c69c2116a6232eeea20
Jason W. Bacon <bacon4000@gmail.com>
2023-01-14 07:09:11 -0600

    Clean up fetch-gff.sh

--- Test/Reference/fetch-gff.sh
+++ Test/Reference/fetch-gff.sh
@@ -11,20 +11,32 @@ gff=$(Reference/gff-filename.sh)
 # macOS zcat looks for .Z extension, while Linux does not have gzcat
 zcat='gunzip -c'
 
-# GFF
-# Can't guarantee this file or the chromosome files will always be available.
-# You may need to edit this.
+##########################################################################
+# Ensembl combined GFFs are sorted lexically by chromosome, while BAMs are
+# sorted numerically.  Build our own GFF by concatenating individual
+# chromosome GFFs in numeric order.  Resorting a GFF is complicated due
+# to the hierarchical sort order (all gene components directly under the
+# gene, etc).
+##########################################################################
+
 cd Data/04-reference
 rm -f $gff
-for chrom in I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI; do
-    site=http://ftp.ensembl.org/pub/release-$release/gff3/saccharomyces_cerevisiae
+site=http://ftp.ensembl.org/pub/release-$release/gff3/saccharomyces_cerevisiae
+
+# Keep header from first GFF.
+file=Saccharomyces_cerevisiae.R64-1-1.106.chromosome.I.gff3.gz
+if [ ! -e $file ]; then
+    printf "Fetching $file...\n"
+    $fetch $site/$file
+fi
+$zcat $file | egrep '^##gff|^#!' | blt deromanize 1 > $gff
+
+# Concatenate the rest without the header
+for chrom in II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI; do
     file=Saccharomyces_cerevisiae.R64-1-1.106.chromosome.$chrom.gff3.gz
     if [ ! -e $file ]; then
-	printf "Fetching $gff.gz...\n"
+	printf "Fetching $file...\n"
 	$fetch $site/$file
-    fi
-    if [ $chrom = I ]; then
-	$zcat $file | egrep '^##gff|^#!' | blt deromanize 1 > $gff
     fi
     $zcat $file | egrep -v '^##[a-z]|^#!' | blt deromanize 1 >> $gff
 done
