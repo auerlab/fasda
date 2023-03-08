09cbc850ebf3298ce1c094d10ec0d9d6f3b40086
Jason W. Bacon <bacon4000@gmail.com>
2023-03-08 15:03:06 -0600

    abundance-variation.sh: Clean up

--- Test/abundance-variation.sh
+++ Test/abundance-variation.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
 cd Data/06-kallisto-quant
+
+# First ten transcripts
 genes=$(head -n 11 SNF2-1/abundance.tsv | tail -10 | cut -f 1)
+
+# All transcripts
+# genes=$(cat SNF2-1/abundance.tsv | cut -f 1)
+
+printf "%s\t%s\t%s\t%s\n" "Transcript" "Min" "Median" "Max"
 for gene in $genes; do
     awk -v gene=$gene '$1 == gene' WT-*/abundance.tsv | cut -f 5 | sort -g > temp
     min=$(head -1 temp)
