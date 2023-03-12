c718805fa53108fb036a1f15de19fe1d217ece40
Jason W. Bacon <bacon4000@gmail.com>
2023-03-12 08:13:13 -0500

    yeast-test.sh: Print header before each step

--- Test/yeast-test.sh
+++ Test/yeast-test.sh
@@ -17,6 +17,14 @@ usage()
 }
 
 
+header()
+{
+    printf "\n=============================================================\n"
+    printf "$1\n"
+    printf "=============================================================\n\n"
+}
+
+
 ##########################################################################
 #   Main
 ##########################################################################
@@ -32,60 +40,69 @@ if [ $max_replicates -lt 3 ]; then
 fi
 
 cd ..
-./cave-man-install.sh
+./cave-man-install.sh || true
 cd Test
 
 ./00-organize.sh
 ./01-fetch.sh $max_replicates
 
 # FIXME: Let 02-trim.sh handle the count check?
-raw_count=$(ls Data/01-fetch/Raw-renamed/*.gz | wc -l)
-trimmed_count=$(ls Data/02-trim/*.gz | wc -l)
+raw_count=$(ls Data/01-fetch/Raw-renamed/*.gz 2> /dev/null | wc -l)
+trimmed_count=$(ls Data/02-trim/*.gz 2> /dev/null | wc -l)
 if [ $trimmed_count -lt $raw_count ]; then
+    header "Trimming raw reads..."
     ./02-trim.sh
 else
-    printf "02-trim.sh already done.\n"
+    header "02-trim.sh already done."
 fi
 
-qc_count=$(ls Data/03-qc/02-trim/*.zip | wc -l)
+qc_count=$(ls Data/03-qc/02-trim/*.zip 2> /dev/null | wc -l)
 if [ $qc_count -ne $raw_count ]; then
+    header "Running FastQC quality checks..."
     ./03-qc.sh
 else
-    printf "03-qc.sh already done.\n"
+    header "03-qc.sh already done."
 fi
 
 if [ ! -e Data/04-reference/all-but-xy.genome.fa.fai ]; then
+    header "Building reference genome / transcriptome..."
     ./04-reference.sh
 else
-    printf "04-reference.sh already done.\n"
+    header "04-reference.sh already done."
 fi
 
 if [ ! -e Data/05-kallisto-index/all-but-xy.index ]; then
+    header "Building kallisto index..."
     ./05-kallisto-index.sh
 else
-    printf "05-kallisto-index.sh already done.\n"
+    header "05-kallisto-index.sh already done."
 fi
 
-quant_count=$(ls -d Data/06-kallisto-quant/* | wc -l)
+quant_count=$(ls -d Data/06-kallisto-quant/* 2> /dev/null | wc -l)
 if [ $quant_count -ne $raw_count ]; then
+    header "Running kallisto transcriptome alignment / quantification..."
     ./06-kallisto-quant.sh
 else
-    printf "06-kallisto-quant.sh already done.\n"
+    header "06-kallisto-quant.sh already done."
 fi
 
+header "Running FASDA differential analysis on kallisto abundances..."
 ./07-fasda-kallisto.sh $max_replicates
 
 if [ ! -e Data/08-hisat-index/all-but-xy.index ]; then
+    header "Building hisat2 index..."
     ./08-hisat-index.sh
 else
-    printf "08-hisat-index.sh already done.\n"
+    header "08-hisat-index.sh already done."
 fi
 
-quant_count=$(ls -d Data/09-hisat-align/* | wc -l)
+quant_count=$(ls -d Data/09-hisat-align/* 2> /dev/null | wc -l)
 if [ $quant_count -ne $raw_count ]; then
+    header "Running hisat2 genome alignment..."
     ./09-hisat-align.sh
 else
-    printf "09-hisat-align.sh already done.\n"
+    header "09-hisat-align.sh already done."
 fi
 
+header "Running FASDA differential analysis on hisat2 alignments..."
 ./10-fasda-hisat.sh $max_replicates
