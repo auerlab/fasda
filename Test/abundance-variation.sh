f498eaddc844ff2732e38042da9c06ffcaee5706
Jason W. Bacon <bacon4000@gmail.com>
2023-03-22 10:08:31 -0500

    abundance-variation.sh: Show spread, align numbers

--- Test/abundance-variation.sh
+++ Test/abundance-variation.sh
@@ -8,11 +8,12 @@ genes=$(head -n 11 SNF2-1/abundance.tsv | tail -10 | cut -f 1)
 # All transcripts
 # genes=$(cat SNF2-1/abundance.tsv | cut -f 1)
 
-printf "%s\t%s\t%s\t%s\n" "Transcript" "Min" "Median" "Max"
+printf "%s\t%6s\t%6s\t%6s\t%6s\n" "Transcript" "Min" "Median" "Max" "Spread"
 for gene in $genes; do
     awk -v gene=$gene '$1 == gene' WT-*/abundance.tsv | cut -f 5 | sort -g > temp
     min=$(head -1 temp)
     median=$(head -24 temp | tail -1)
     max=$(tail -1 temp)
-    printf "%s\t%s\t%s\t%s\n" $gene $min $median $max
+    spread=$(echo "scale=1; $max / $min" | bc -l)
+    printf "%s\t%6.1f\t%6.1f\t%6.1f\t%6.1f\n" $gene $min $median $max $spread
 done
