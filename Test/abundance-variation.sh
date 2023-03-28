87ba62953baf1570206fe7c4c1f6dedd4a2c3cbc
Jason W. Bacon <bacon4000@gmail.com>
2023-03-28 08:24:46 -0500

    abundance-variation.sh: Use normalized abundances

--- Test/abundance-variation.sh
+++ Test/abundance-variation.sh
@@ -3,14 +3,14 @@
 cd Results/06-kallisto-quant
 
 # First ten transcripts
-genes=$(head -n 11 WT-1/abundance.tsv | tail -10 | cut -f 1)
+genes=$(head -n 11 WT-1/normalized.tsv | tail -10 | cut -f 1)
 
 # All transcripts
 # genes=$(cat SNF2-1/abundance.tsv | cut -f 1)
 
 printf "%s\t%6s\t%6s\t%6s\t%7s\n" "Transcript" "Min" "Median" "Max" "Max/Min"
 for gene in $genes; do
-    awk -v gene=$gene '$1 == gene' WT-*/abundance.tsv | cut -f 5 | sort -g > temp
+    awk -v gene=$gene '$1 == gene' WT-*/normalized.tsv | cut -f 6 | sort -g > temp
     # Use pipe to eliminate filename from output
     samples=$(cat temp | wc -l)
     odd=$(($samples % 2))
@@ -25,7 +25,8 @@ for gene in $genes; do
 	median=$(echo "scale=2; ($m1 + $m2) / 2" | bc -l)
     fi
     max=$(tail -1 temp)
-    if [ $min != 0 ]; then
+    intmin=${min%.*}
+    if [ $intmin != 0 ]; then
 	spread=$(echo "scale=1; $max / $min" | bc -l)
 	printf "%s\t%6.1f\t%6.1f\t%6.1f\t%7.1f\n" $gene $min $median $max $spread
     fi
