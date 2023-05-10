f02421a3c208d96a165d98031037130be5837158
Jason W. Bacon <bacon4000@gmail.com>
2023-05-10 07:08:29 -0500

    random-sample-fcs.sh: Add code to generate random sets of samples

--- Test/random-sample-fcs.sh
+++ Test/random-sample-fcs.sh
@@ -20,6 +20,38 @@ usage()
 
 
 ##########################################################################
+#   Function description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#       
+#   History:
+#   Date        Name        Modification
+#   2023-05-10  Jason Bacon Begin
+##########################################################################
+
+random()
+{
+    if [ $# != 1 ]; then
+	printf "Usage: random sample-count\n"
+	exit 1
+    fi
+    modulus=$1
+    
+    taken=true
+    while [ $taken = true ]; do
+	random=$(head /dev/urandom | cksum | cut -d ' ' -f 1)
+	random=$(($random % $modulus))
+	if [ ! -e taken.$random ]; then
+	    taken=false
+	    touch taken.$random
+	fi
+    done
+    printf "$random\n"
+}
+
+##########################################################################
 #   Main
 ##########################################################################
 
@@ -28,5 +60,21 @@ if [ $# != 1 ]; then
 fi
 sample_count=$1
 
-# Results/06-kallisto-quant/*/abundance.tsv
+# Compare 10 random selections of samples
+for trial in $(seq 1 10); do
 
+    # Choose $sample_count biological samples at random from the 48
+    # yeast samples
+    rm -f taken.*
+    for n in $(seq 1 $sample_count); do
+	sample=$(random 48)         # 0-based
+	sample=$(($sample + 1))     # 1-based
+	wt=Results/06-kallisto-quant/WT-$sample/abundance.tsv
+	snf2=Results/06-kallisto-quant/SNF2-$sample/abundance.tsv
+	echo $wt $snf2
+    done
+    rm -f taken.*
+    
+    # Compute fold-changes and P-values for this set of samples
+    
+done
