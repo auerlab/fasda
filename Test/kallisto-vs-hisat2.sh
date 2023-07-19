a3fbc2ba917eb162373ebad66ab69a81589bfd59
Jason W. Bacon <bacon4000@gmail.com>
2023-07-19 14:58:45 -0500

    kallisto-vs-hisat2.sh: Compare raw and normalized abundances

--- Test/kallisto-vs-hisat2.sh
+++ Test/kallisto-vs-hisat2.sh
@@ -15,11 +15,21 @@ pause()
 }
 
 # Abundances
+kallisto=Results/06-kallisto-quant/WT-1/abundance.tsv
+hisat2=Results/09-hisat-align/WT-1-abundance.tsv
+
+for transcript in $(awk '{ print $1 }' $kallisto); do
+    printf '===\n'
+    printf "Kallisto: "
+    grep $transcript $kallisto
+    printf "Hisat2:   "
+    grep $transcript $hisat2
+done | more
+
+# Normalized
 kallisto=Results/07-fasda-kallisto/SNF2-all-norm-03.tsv
 hisat2=Results/10-fasda-hisat/SNF2-all-norm-03.tsv
 
-#kallisto=Results/06-kallisto-quant/WT-1/abundance.tsv
-#hisat2=Results/09-hisat-align/WT-1-abundance.tsv
 for transcript in $(awk '{ print $1 }' $kallisto); do
     printf '===\n'
     printf "Kallisto: "
