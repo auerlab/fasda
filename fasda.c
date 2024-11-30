2c104c221a571b52f1630ad59eb3a29cdf434257
Jason W. Bacon <bacon4000@gmail.com>
2024-11-30 08:04:07 -0600

    Use argv[0] for program name in --version

--- fasda.c
+++ fasda.c
@@ -51,7 +51,7 @@ int     main(int argc,char *argv[])
     
     if ( (argc == 2) && (strcmp(argv[1],"--version") == 0) )
     {
-	printf("fasda %s\n", VERSION);
+	printf("%s %s\n", argv[0], VERSION);
 	return EX_OK;
     }
     else if ( argc < 2 )
