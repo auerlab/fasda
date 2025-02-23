3da6671b109cc554847debed422941a10d11267b
Jason W. Bacon <bacon4000@gmail.com>
2025-02-23 11:35:05 -0600

    13b-stats.sh: Show % with P < 0.05

--- Yeast-test/LPJS/13b-stats.sh
+++ Yeast-test/LPJS/13b-stats.sh
@@ -2,8 +2,11 @@
 
 printf "Total features:\n"
 wc -l Results/13-fasda-kallisto/*.txt
+total=$(wc -l Results/13-fasda-kallisto/*.txt | awk '{ print $1 }')
 
 printf "\nFeatures with P-values < 0.05:\n"
 for file in Results/13-fasda-kallisto/*.txt; do
-    printf "$(awk '$8 < 0.05' $file | wc -l) $file\n"
+    sig=$(awk '$8 < 0.05' $file | wc -l | awk '{ print $1 }')
+    percent=$(printf "scale=2\n100 * $sig / $total\n" | bc)
+    printf "$(awk '$8 < 0.05' $file | wc -l) ($percent%%) $file\n"
 done
